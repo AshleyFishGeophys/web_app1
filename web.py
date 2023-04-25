c3ff4e3b5c3951ece82b4977c784aeff002cd3ce
Ashley Yaner <ashley.yaner@fractureid.com>
2023-04-25 16:42:05 -0600

    web app1 initial commit. requirements.txt generated. pip freeze > rewuirements.txt

--- web.py
+++ web.py
@@ -1,8 +1,6 @@
 import streamlit as st
-from modules.functions import *
+from functions import *
 
-
-FILEPATH = "files/todos.txt"
 
 def add_todo():
     todo = st.session_state["new_todo"] + "\n"
@@ -10,17 +8,17 @@ def add_todo():
     write_todos(todos, FILEPATH)
 
 
-todos = get_todos(FILEPATH)
+todos = get_todos()
 
 st.title("My Honey-Do List App")
 # st.subheader("This is my todo app.")
-st.write("This app is to help you keep track of your Honey-Do items!")
+st.write("This app is to help you keep track of your Honey-Do 'requests'!")
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
     if checkbox:
         todos.pop(index)
-        write_todos(todos, FILEPATH)
+        write_todos(todos)
         del st.session_state[todo]
         # st.rerun()
         st.experimental_rerun()
@@ -30,8 +28,6 @@ st.text_input(
     placeholder="Add new todo...",
     on_change=add_todo,
     key="new_todo")
-
-print("Hello")  # prints in the terminal, not in the web app
 
 # The session_state will show everything that has keys
 st.session_state
